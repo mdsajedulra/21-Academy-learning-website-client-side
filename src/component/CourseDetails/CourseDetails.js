e5fef2e6773570ba37126b14c6ee0a3de848a60e
mdsajedulra <mdsajedulra@gmail.com>
2022-10-25 21:57:19 +0600

    details page left side complete

--- src/component/CourseDetails/CourseDetails.js
+++ src/component/CourseDetails/CourseDetails.js
@@ -6,22 +6,34 @@ const CourseDetails = () => {
     console.log(details)
     const { title, instructor, thumbnail_url, total_enrollment, rating, price, overview } = details
     return (
-        <div className='flex'>
-            <div className='w-2/4'>
-                <h1>{title}</h1>
+        <div className='flex gap-10 mt-10'>
+            <div className='flex flex-col w-2/4 gap-10'>
+                <h1 className='text-5xl'>{title}</h1>
                 <p>{details.details}</p>
+
                 <div>
-                    <img src={instructor.img} alt="" />
-                    <p>{instructor.name}</p>
+                    <h1 className='text-3xl '>Instructor</h1>
+                    <div className='flex items-center gap-2 p-4 border rounded-md'>
+
+                        <img className='rounded-full w-14' src={instructor.img} alt="" />
+                        <div>
+                            <p>{instructor.name}</p>
+                            <p>Instuctor, 21 Academy; Bestselling Author</p>
+                        </div>
+                    </div>
                 </div>
-                <div >
-                    {
-                        overview.map(item => <li>{item}</li>)
-                    }
+                <div>
+                    <h1 className='text-3xl '>What you will learn from this course</h1>
+                    <div className='p-4 border rounded-md'>
+                        {
+                            overview.map(item => <li>{item}</li>)
+                        }
+                    </div>
                 </div>
             </div>
             <div className='w-2/4'>
                 <img src={instructor.img} alt="" />
+
             </div>
         </div>
     );
