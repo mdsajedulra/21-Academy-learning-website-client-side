7b19790a2d575d8f9ecf0bc5f075da0017b39864
mdsajedulra <mdsajedulra@gmail.com>
2022-10-25 20:31:11 +0600

    course details page data laod

--- src/component/CourseDetails/CourseDetails.js
+++ src/component/CourseDetails/CourseDetails.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const CourseDetails = () => {
+    const details = useLoaderData()
+    console.log(details)
+    const { title, instructor, thumbnail_url, total_enrollment, rating, price, overview } = details
     return (
-        <div>
-            this is course
+        <div className='flex'>
+            <div className='w-2/4'>
+                <h1>{title}</h1>
+                <p>{details.details}</p>
+                <div>
+                    <img src={instructor.img} alt="" />
+                    <p>{instructor.name}</p>
+                </div>
+                <div >
+                    {
+                        overview.map(item => <li>{item}</li>)
+                    }
+                </div>
+            </div>
+            <div className='w-2/4'>
+                <img src={instructor.img} alt="" />
+            </div>
         </div>
     );
 };
